8948c101ed3472e81d577a296492152ae02b968d
JerryChenZeyun <mrtaller@gmail.com>
2020-03-14 15:58:05 +1100

    Add files via upload

--- health_org_demo.py
+++ health_org_demo.py
@@ -1,4 +1,4 @@
-##########################################################################################
+####################################################################################################################################################################################
 # Script Function: Demonstrate AWS Health API for Organization View
 # Author: JC
 # Time: 2020.03.08
@@ -6,7 +6,7 @@
 # Execution requirements: 
 #   1. Update the "accountId" value following the instruction from Step 10 in  https://github.com/JerryChenZeyun/aws-health-api-organization-view/blob/master/README.md#setup
 #   2. Update the "bucketName" value following the instruction from Step 10 in  https://github.com/JerryChenZeyun/aws-health-api-organization-view/blob/master/README.md#setup
-##########################################################################################
+####################################################################################################################################################################################
 
 import boto3
 import logging
@@ -14,9 +14,11 @@ from botocore.exceptions import ClientError
 import json
 import datetime
 import pandas as pd
+import os
 
-accountId = '123456789012'
+####################################################################################################################################################################################
 bucketName = 'my-test-bucket-20191011'
+####################################################################################################################################################################################
 
 arn_list = []
 service_list = []
@@ -29,7 +31,12 @@ lastUpdatedTime_list = []
 statusCode_list = []
 
 # Transform dict format into list format
-def dict_to_list(event_data):
+def dict_to_list():
+    client = boto3.client('health')
+    event_data = client.describe_events_for_organization(
+        filter={
+        }
+    )
     for i in range(0,len(event_data["events"])):
         arn_list.append(event_data["events"][i]["arn"])
         service_list.append(event_data["events"][i]["service"])
@@ -93,32 +100,24 @@ def create_manifest():
     """Create a Manifest file to an user specified S3 bucket
     This is required when user need to visualise the event data hosted in S3 bucket via QuickSight
     """
-    #f = open("/home/ec2-user/boto-test/manifest.json", "r")
-    #for line in f:
-    #    f.write(line.replace('bucket-name', bucketName))
-    #print(f.read())
-    with open("/home/ec2-user/environment/aws-health-api-organization-view/manifest.json", "rt") as fin:
-        with open("/home/ec2-user/environment/manifests3.json", "wt") as fout:
+    dirpath = os.getcwd()
+    file_path_ori = dirpath + "/manifest.json"
+    file_path_new = dirpath + "/manifests3.json"
+
+    with open(file_path_ori, "rt") as fin:
+        with open(file_path_new, "wt") as fout:
             for line in fin:
                 fout.write(line.replace('bucket-name', bucketName))
 
-
-# Main part of Script
-# -------------------
-if __name__ == "__main__":  
-
-
+## Enable the health service in organization level
+def enable_health_org():
     client = boto3.client('health')
-
     try:
         response = client.enable_health_service_access_for_organization()
         print("enable health api at organization level success")
     except ClientError as e:
         logging.error(e)
         print("enable health api at organization level error occurs:", e)
-
-    ## Enable the health service in organization level
-    
     
     print("\n#########################################################################\n")
     print(response)
@@ -126,82 +125,105 @@ if __name__ == "__main__":
     print("Health Service has been enabled at AWS Organization Level -- Done!")
     print("\n#########################################################################\n")
 
-    ## describe_health_service_status_for_organization
+## describe_health_service_status_for_organization
+def describe_health_service_status_for_org():
+    client = boto3.client('health')
     response = client.describe_health_service_status_for_organization()
     print(response)
     print("\n#########################################################################\n")
 
-    ## describe_events_for_organization(**kwargs)
+## describe_events_for_organization(**kwargs)
+def describe_events_for_org():
+    client = boto3.client('health')
     response_org_event = client.describe_events_for_organization(
         filter={
-
         }
     )
-
     print("\n#########################################################################\n")
     print("describe_events_for_organization response is \n")
     print("\n#########################################################################\n")
     print(response_org_event)
-    #print(type(response_org_event["events"][0]["arn"]))
-    #print(response_org_event["events"][0]["arn"])
+
+## describe_affected_accounts
+def describe_affected_accounts():
+    client = boto3.client('health')
+    ## iterate the event list to retrieve affected account info for each health event
+    print("\n#########################################################################\n")
+    ## retrieve health event data
+    response = client.describe_events_for_organization(
+        filter={
+        }
+    )
+    event_arn_list = []
+    for i in range(0,len(response["events"])):
+        event_arn_list.append(response["events"][i]["arn"])
+        print("\n---------------------------------------------------------------------\n")
+        print("affected accounts for health event: " + event_arn_list[i])
+        response_account = client.describe_affected_accounts_for_organization(eventArn = event_arn_list[i])
+        print(response_account)
     print("\n#########################################################################\n")
 
+## Retrieve account id automatically
+## NOT in use for this Lab
+def get_account_id():
+    return(boto3.client('sts').get_caller_identity().get('Account'))
 
-    ## describe_affected_accounts_for_organization(**kwargs)
-    ## ["events"][1]["arn"] -- This is the EC2 event in Jan
-
-    #ec2_event_arn = "arn:aws:health:ap-southeast-2::event/EC2/AWS_EC2_OPERATIONAL_ISSUE/AWS_EC2_OPERATIONAL_ISSUE_RFMRO_1579740075"
-    ec2_event_arn = "arn:aws:health:global::event/CLOUDFRONT/AWS_CLOUDFRONT_OPERATIONAL_ISSUE/AWS_CLOUDFRONT_OPERATIONAL_ISSUE_HQTPY_1580371190"
-    ##ec2_event_arn = response["events"][1]["arn"] 
-    print(ec2_event_arn)
-
-    response = client.describe_affected_accounts_for_organization(
-        eventArn = ec2_event_arn
-        #nextToken = 'string',
-        #maxResults = 123
-        )
-    print("\n#########################################################################\n")
-    print("Affected accounts for this event are: \n")
-    print(response)
-    print("\n#########################################################################\n")
-
-
-    ## describe_events_details_for_organization(**kwargs) 
-
+## describe_events_details_for_organization(**kwargs) 
+## NOT in use for this Lab
+def describe_affected_event_details(event_arn):
+    client = boto3.client('health')
     response = client.describe_event_details_for_organization(
         organizationEventDetailFilters=[
             {
-                'eventArn': ec2_event_arn,
+                'eventArn': event_arn,
                 'awsAccountId': accountId
             },
-        ]#,
-        #locale='string'
+        ]
     )
-
     print("\n#########################################################################\n")
     print("Destribe event details for specific account in organization \n")
     print(response)
     print("\n#########################################################################\n")
 
-
-
-    ## describe_affected_entities_for_organization(**kwargs)
+## describe_affected_entities_for_organization(**kwargs)
+## NOT in use for this Lab
+def describe_affected_entities(event_arn):
+    client = boto3.client('health')
     response = client.describe_affected_entities_for_organization(
         organizationEntityFilters=[
             {
-                'eventArn': ec2_event_arn,
+                'eventArn': event_arn,
                 'awsAccountId': accountId
             },
         ],
     )
-
     print("\n#########################################################################\n")
     print("Destribe affected entities for specific account in organization \n")
     print(response)
     print("\n#########################################################################\n")
 
+
+
+# Main part of Script
+# -------------------
+if __name__ == "__main__":  
+
+    client = boto3.client('health')
+
+    ## Enable the health service in organization level
+    enable_health_org()
+
+    ## describe_health_service_status_for_organization
+    describe_health_service_status_for_org()
+
+    ## describe_events_for_organization(**kwargs)
+    describe_events_for_org()
+
+    ## describe_affected_accounts
+    describe_affected_accounts()
+
     ## transfer dict data to list
-    dict_to_list(response_org_event)
+    dict_to_list()
     
     ## save event data to csv file
     write_to_csv()
@@ -212,5 +234,4 @@ if __name__ == "__main__":
     ## create manifest file, and save it to S3 bucket
     create_manifest()
     upload_to_s3(file_name = "manifests3.json", bucket = bucketName, key = "manifests3.json")
-
 
